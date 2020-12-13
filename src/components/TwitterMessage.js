629dade647509975f320ac892079546dfb476d2f
jzolo22 <jzolotarev@gmail.com>
2020-12-13 18:00:59 -0500

    done with original twitter solution

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,24 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
 
-    this.state = {};
+  state = {
+    message: "",
+    charsLeft: 280
   }
+
+  handleType = event => {
+    console.log(event.target.value)
+    this.setState({message: event.target.value, charsLeft: this.state.charsLeft - 1 })
+  }
+
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} onChange={this.handleType} />
+        <h4>{this.state.charsLeft} characters left</h4>
       </div>
     );
   }
